da1eeef23415bbd446e4930bfcf255930bfc80f9
varshanookarapu <lakshmivarsha.n@gmail.com>
2022-10-14 15:33:31 +0530

    Added Card Stats, changed card box shadow and added card styles

--- src/lib/TvShow.svelte
+++ src/lib/TvShow.svelte
@@ -1,8 +1,25 @@
-<div class="card" style="width: 18rem;">
-  <img src="..." class="card-img-top" alt="..." />
+<script>
+  export let show;
+  console.log(show);
+</script>
+
+<div class="card">
+  <img src={show.image.medium} class="card-img-top" alt="..." />
   <div class="card-body">
-    <h5 class="card-title">TV Show Image</h5>
-    <p class="card-text">Stats</p>
+    <h5 class="card-title">{show.name}</h5>
+    {#each show.genres as genre}
+      <span class="badge text-bg-primary me-1">{genre}</span>
+    {/each}
+    <p class="card-text">{show.language}</p>
+    <span>Rating: {show.rating.average}</span>
     <a href="#" class="btn btn-primary">Go somewhere</a>
   </div>
 </div>
+
+<style>
+  .card {
+    width: 18rem;
+    box-shadow: rgb(0 0 0) 6px 7px 11px;
+    border: 0;
+  }
+</style>
